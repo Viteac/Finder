1b95244b8abe8a9673dfaa3e8c271f4599c7605e
Vimuster <32931324+Viteac@users.noreply.github.com>
2021-02-11 15:32:28 +0000

    Update finder.py

--- finder.py
+++ finder.py
@@ -3,28 +3,21 @@ import os
 founded = []
 
 def just_dir(dirname,text):
-    # extension = input('Enter the file extension\n>>')
     m = os.listdir(dirname)
     print(m)
     for file in m:
-        # print(file)
-        # if file.endswith('extension'):
         if os.path.isfile(f'{dirname}{file}'):
             with open(f'{dirname}{file}', encoding='utf-8', errors='ignore') as f:
                 read = f.read()
                 if text in read:
-                    #print('here', file)
                     founded.append(f'{dirname}{file}')
 
 def subdirectory(dirname,text):
     for root, dirs, files in os.walk(dirname):
         for name in files:
-            # print(name)
-            #if name.endswith('.txt') or name.endswith('.py'):
             with open(os.path.join(root, name), encoding='utf-8', errors='ignore') as f:
                 read = f.read()
             if text in read:
-                #print('here', root, name)
                 founded.append(f'{root}{name}')
 def checkdir():
     dirname: str = input('Enter the file name \n >> ')
